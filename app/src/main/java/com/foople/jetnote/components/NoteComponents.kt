35870e50849397c5c646ca9ef3278096ef1b3470
Johan Andres Uribe <johanandresuribe@Johans-MacBook-Pro.local>
2022-10-08 14:03:27 -0400

    Initial commit

---
+++ app/src/main/java/com/foople/jetnote/components/NoteComponents.kt
@@ -0,0 +1,50 @@
+package com.foople.jetnote.components
+
+import androidx.compose.foundation.text.KeyboardActions
+import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.material.Text
+import androidx.compose.material.TextField
+import androidx.compose.material.TextFieldDefaults
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.ExperimentalComposeUiApi
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalSoftwareKeyboardController
+import androidx.compose.ui.text.input.ImeAction
+import androidx.compose.ui.text.input.TextFieldValue
+
+@OptIn(ExperimentalComposeUiApi::class)
+@Composable
+fun NoteInputText(
+    modifier: Modifier = Modifier,
+    text: String,
+    label: String,
+    maxLine: Int = 1,
+    onTextChange: (TextFieldValue) -> Unit,
+    onImeAction: () -> Unit = {}
+) {
+    val keyboardController = LocalSoftwareKeyboardController.current
+
+    TextField(
+        value = TextFieldValue(text = text),
+        onValueChange = onTextChange,
+        colors = TextFieldDefaults.textFieldColors(
+            backgroundColor = Color.Transparent
+        ),
+        maxLine = maxLine,
+        label = NoteInputLabel(label),
+        keyboardOptions = KeyboardOptions.Default.copy(
+            imeAction = ImeAction.Done
+        ),
+        keyboardActions = KeyboardActions(onDone = {
+            onImeAction()
+            keyboardController?.hide()
+        }),
+        modifier = modifier
+    )
+}
+
+@Composable
+fun NoteInputLabel(label: String): Unit {
+    Text(text = label)
+}
---
+++ app/src/main/java/com/foople/jetnote/screen/NoteScreen.kt
@@ -0,0 +1,44 @@
+package com.foople.jetnote.screen
+
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.Icon
+import androidx.compose.material.Text
+import androidx.compose.material.TopAppBar
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.rounded.Notifications
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.foople.jetnote.R
+
+@Composable
+fun NoteScreen() {
+    Column(modifier = Modifier.padding(6.dp)) {
+        TopAppBar(
+            title = { Text(text = stringResource(id = R.string.app_name)) },
+            actions = {
+                Icon(imageVector = Icons.Rounded.Notifications, contentDescription = "Icon")
+            },
+            backgroundColor = Color(0xFFDADFE3)
+        )
+
+        Column(
+            modifier = Modifier.fillMaxWidth(),
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+
+        }
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun NotesScreenPreview() {
+    NoteScreen()
+}
